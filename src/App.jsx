31c9ee4793bab4263b720f2a1f4f9c430b00e740
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2025-10-03 10:05:49 +0100

    feat:Add more routes and fix Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,87 @@
 import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+
+// Components
+import ProtectedRoute from "./Components/ProtectedRoute";
+import LoadingSpinner from "./Components/LoadingSpinner";
+
+// Pages
 import Home from './Pages/Home';
+import Movies from "./Pages/Movies";
+import Signin from "./Pages/Signin";
+import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
+import MovieDetail from "./Pages/MovieDetail";
 import NotFound from './Pages/NotFound';
+import Upcoming from "./Pages/Upcoming";
+import Favorite from "./Pages/Favorite";
 
 
-function App() {
-
+function AppContent() {
   return (
+    <div className="App">
     <Routes>
+      {/* Public routes */}
       <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<div>Movie Page</div>} />
-      <Route path="/contact" element={<div>Contact Page</div>} />
-      <Route path="/upcoming" element={<div>Upcoming Page</div>} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movie/:id" element={<MovieDetail />} />
+      <Route path="/upcoming" element={<Upcoming />} />
+
+      {/* Auth routes - redirect to home if already authenticated */}
+      <Route 
+        path="/signin"
+        element={
+          <ProtectedRoute requireAuth={false}>
+            <Signin />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route 
+        path="/signup"
+        element={
+          <ProtectedRoute requireAuth={false}>
+            <SignUp />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Protected routes - require authentication */}
+      <Route 
+        path="/profile"
+        element={
+          <ProtectedRoute requireAuth={true}>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route 
+        path="/favorites"
+        element={
+          <ProtectedRoute requireAuth={true}>
+            <Favorite />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Catch all route */}
       <Route path='*' element={<NotFound />} />
     </Routes>
+    </div>
   )
 }
 
-export default App
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<LoadingSpinner />} persistor={persistor} >
+        <AppContent />
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default App;
